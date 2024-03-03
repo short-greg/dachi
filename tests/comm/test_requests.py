4d57fa4f5189b1fbf827a41a7fa8ea057e20b4cd
Short <gshort23@yahoo.com>
2024-03-03 16:01:41 +0900

    refactor requests and simplified them

--- tests/comm/test_requests.py
+++ tests/comm/test_requests.py
@@ -1,6 +1,6 @@
 from dachi.comm import Post, Request
 from dachi.comm._query import Post
-
+import time
 
 class DummyQuery(Request):
 
@@ -10,12 +10,11 @@ class DummyQuery(Request):
     
     def post(self, post: Post):
 
-        print('Calling')
         self.called = True
         post.response = 'RESPONDED'
 
 
-class TestQuest:
+class TestRequest:
 
     def test_query_prepares_post(self):
 
@@ -38,39 +37,11 @@ class TestQuest:
         post.request(query)
         assert post.response == "RESPONDED"
 
+    # # Test the threaded version turned off cause it is slow
+    # def test_content_is_correct_after_calling_threaded(self):
 
-# class TestSignal:
-
-#     def test_query_prepares_post(self):
-
-#         signal = DummySignal()
-#         signal.register(signal.on_post)
-#         signal.post(Request('signal'))
-#         assert signal.called is True
-
-#     def test_post_is_called(self):
-
-#         signal = DummySignal()
-#         request = Request('signal')
-#         request.on_post = signal.on_post
-#         signal.post(request)
-#         assert signal.called is True
-
-
-
-# class DummySignal(Signal):
-
-#     def __init__(self):
-#         super().__init__()
-#         self.called = False
-#         self.post_it = False
-
-#     def prepare_response(self, request):
-#         return 'respond!'
-    
-#     def prepare_post(self, request):
-
-#         self.post_it = True
-
-#     def on_post(self, request):
-#         self.called = True
+    #     query = DummyQuery()
+    #     post = Post()
+    #     post.threaded_request(query)
+    #     time.sleep(1)
+    #     assert post.response == "RESPONDED"
