d1148399bee02e89abb6761394f1b2bbda5dd0cd
Short <gshort23@yahoo.com>
2025-09-03 18:54:01 +0900

    removed adapt package

--- dachi/adapt/xopenai/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._openai import (
-    ChatCompletion, LLM,
-    to_openai_tool
-)
-
-# Unified response processors are now available from dachi.proc._resp:
-# TextConv, StructConv, ParsedConv, ToolConv, StructStreamConv
-# Tool execution converter is available from dachi.proc._out:
-# ToolExecConv
